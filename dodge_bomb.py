f589ba961cf42de00af053540d4132f2ce68f154
C0A23013 <c0a230137a@edu.teu.ac.jp>
2024-11-26 14:52:10 +0900

    練習3

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -1,7 +1,8 @@
 import os
+import random
 import sys
 import pygame as pg
-import random
+
 
 
 WIDTH, HEIGHT = 1100, 650
@@ -12,6 +13,15 @@ DELTA = {
         pg.K_LEFT: (-5, 0),
         pg.K_RIGHT: (5, 0),
     }
+def check_bound(obj_rct):
+    """
+    引数：Rectオブジェクト
+    戻り値：横方向・縦方向の真理値タプル（True：画面内／False：画面外）
+    """
+    x_within = (0 <= obj_rct.left) and (obj_rct.right <= WIDTH)
+    y_within = (0 <= obj_rct.top) and (obj_rct.bottom <= HEIGHT)
+    return x_within, y_within
+
 
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
@@ -22,7 +32,6 @@ def main():
     kk_rct.center = 300, 200
     clock = pg.time.Clock()
     tmr =0
-
     # Create bomb surface
     bb_img = pg.Surface((20, 20)) #爆弾用の空SurFace
     pg.draw.circle(bb_img, (255, 0, 0), (10, 10), 10)  # 赤い円を描画
@@ -31,10 +40,7 @@ def main():
     bb_rct.center = random.randint(0, WIDTH), random.randint(0, HEIGHT)  # ランダムな位置に配置
 
     vx, vy = 5, 5
-    
-
-
-    
+        
 
 
     while True:
@@ -51,8 +57,26 @@ def main():
                 sum_mv[1] += tpl[1]
 
         kk_rct.move_ip(sum_mv)
+        #練習3
+        yoko, tate = check_bound(kk_rct)
+        if not yoko:  # 横方向が画面外の場合
+            kk_rct.left = max(0, kk_rct.left)
+            kk_rct.right = min(WIDTH, kk_rct.right)
+        if not tate:  # 縦方向が画面外の場合
+            kk_rct.top = max(0, kk_rct.top)
+            kk_rct.bottom = min(HEIGHT, kk_rct.bottom)
 
         bb_rct.move_ip(vx, vy)
+        yoko, tate = check_bound(bb_rct)
+        if not yoko:  # 横方向が画面外の場合
+            vx = -vx
+        if not tate:  # 縦方向が画面外の場合
+            vy = -vy
+        
+        # 衝突判定
+        if kk_rct.colliderect(bb_rct):  # 衝突していたら
+            print("衝突しました！ゲームを終了します。")
+            return  # main関数を終了
 
         screen.blit(kk_img, kk_rct)
         screen.blit(bb_img, bb_rct)
